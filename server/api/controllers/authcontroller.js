7ce3703b6abf173cf2ca59d2f0d91086e6e59f3d
Garnet Yeates <45212493+Garnet-Yeates@users.noreply.github.com>
2023-05-27 05:33:45 -0400

    Documentation, rewrites, cleanup

--- server/api/controllers/authcontroller.js
+++ server/api/controllers/authcontroller.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt'
-import { authCheckHelper, shouldAttemptAuth } from '../middleware/authCheck.js';
+import { authCheckHelper, deleteSessionSoon, userSuppliedAnyAuth } from '../middleware/authCheck.js';
 import { createClient, findClient } from '../tools/database/tblClientProcedures.js';
-import { createSession, deleteSession } from '../tools/database/tblSessionProcedures.js.js';
+import { createSession } from '../tools/database/tblSessionProcedures.js';
 
 const userNameRegex = /^[a-zA-Z][a-zA-Z0-9_]+$/
 
@@ -16,7 +16,12 @@ export async function authRequiredTest(req, res) {
 
 /**
  * `POST` `/auth/register` 
- * Attempts to register a new user. The fields for this request in `req.body` are as follows
+ * 
+ * ---
+ * 
+ * Attempts to register a new user.
+ * 
+ * ---
  * 
  * `clientName`: 5-32 characters, required
  * 
@@ -24,15 +29,19 @@ export async function authRequiredTest(req, res) {
  * 
  * `password`: 8-16 characters, required
  * 
+ * ---
+ * 
  * `clientName`, `userName`, and `password` all must conform to their respective regular expressions
  * for validation.
- * 
+ *
  * `clientName` must be alphanumeric, spaces and numbers are allowed but special characters are not
  * 
  * `userName` must also be alphanumeric, but it must start with a letter and cannot contain spaces (underscore allowed)
  * 
  * `password` must contain an uppercase letter and a lowercase letter. It must also contain either a number or a special character.
  * Most special characters are allowed but quotation characters (backticks, single, double) quotes are not allowed.
+ * 
+ * ---
  * 
  * This API endpoint will return `200` upon successful registration
  * 
@@ -67,12 +76,15 @@ export async function register(req, res) {
         userNameErrors.push("Username must start with a letter and be alphanumeric (underscore allowed)")
     }
 
-    let user = await findClient({ userName });
-    if (user && user.sqlError) {
-        databaseErrors.push("Error querying database for existing user check")
+    let user;
+    try {
+        user = await findClient({ userName });
+        if (user) {
+            userNameErrors.push("Username is taken")
+        }
     }
-    else if (user) {
-        userNameErrors.push("Username is taken")
+    catch (err) {
+        databaseErrors.push("Error querying database for existing user check")
     }
 
     if (password.length < 10 || password.length > 16) {
@@ -89,8 +101,13 @@ export async function register(req, res) {
 
     let noErrors = () => databaseErrors.length == 0 && clientNameErrors.length == 0 && userNameErrors.length == 0 && passwordErrors.length == 0;
 
-    if (noErrors() && await createClient(clientName, userName, password).sqlError) {
-        errJson.databaseErrors.push("Error inserting new user into the database")
+    if (noErrors) {
+        try {
+            await createClient(clientName, userName, password);
+        }
+        catch (err) {
+            errJson.databaseErrors.push("Error inserting new user into the database")
+        }
     }
 
     databaseErrors.length > 0 && (errJson.databaseErrors = databaseErrors);
@@ -109,15 +126,29 @@ export async function register(req, res) {
 /**
  * `POST` `/auth/login` 
  * 
- * Req.body should look like:
+ * ---
+ * 
+ * Attempts to log in a user. If login is successful, it will insert a new session into the database and set two session cookies in the user's
+ * user's browser. 
+ * 
+ * One cookie is the `auth_jwt` cookie, which is `HTTPOnly`. This cookie contains a signed token from the server whose payload has properties:
+ * `sessionUUID` and `sessionCSRF`. This token claims that the user has a session in the database with the supplied UUID and also links this UUID
+ * to the `sessionCSRF` (which is another unique identifier). 
+ * 
+ * The other cookie is the `auth_csrf` cookie, which is *not* `HTTPOnly` (can be read by browser). Since cross site scripts cannot read cookies from
+ * different domains, this cookie should only be able to be read from the web app domain. We leverage this to add another layer of authentication
+ * where the user must prove that they are able to read the `auth_csrf` cookie. We make it so that on every request, the user must read the `auth_csrf`
+ * cookie and send a header, `headers.auth_csrf` containing the value of the cookie. If their sent header matches the `auth_csrf` cookie and also matches
+ * the `sessionCSRF` signed in the `auth_jwt` cookie, then they are considered to have 'valid authentication' (note that this does not solely constitute authorization
+ * success as it is possible for the session to be expired, canceled, or gone from the database, or a database error can occur)
+ * 
+ * ---
  * 
  * `userName`: 5-16 characters, required
  * 
  * `password`: 8-16 characters, required
  * 
- * If login is successful, it will insert a new session into the database and set the two session cookies in the
- * user's browser (it is up to the user to set the CSRF header on subsequent requests, effectively proving that they
- * can read the cookie)
+ * ---
  * 
  * This endpoint will return a `200` status code upon login success
  * 
@@ -129,11 +160,15 @@ export async function login(req, res) {
     // Possibly deny request if they already have a session
     const { userName, password } = req.body;
 
-    let user = await findClient({ userName });
-    if (!user) {
-        return res.status(404).json({ errorMessage: "Username or password is incorrect" })
+    let user;
+    try {
+        user = await findClient({ userName });
+        if (!user) {
+            return res.status(404).json({ errorMessage: "Username or password is incorrect" })
+        }
     }
-    else if (user.sqlError) {
+    catch (err) {
+        console.log(err)
         return res.status(500).json({ errorMessage: "Error querying database for user" })
     }
 
@@ -151,21 +186,28 @@ export async function login(req, res) {
 /**
  * `GET` `/auth/logout` 
  * 
+ * ---
+ * 
  * Attempts to log out a user. No fields are required for this request since the 3 things we use
- * for authorization (CSRF cookie, JWT cookie, CSRF header) are automatically included in requests.
+ * to check if they are logged in (CSRF cookie, JWT cookie, CSRF header) are automatically included in requests.
  * This API endpoint will never return an error, even if the user wasn't logged in or even if there
  * was an error querying the database to find OR delete the session (realistically we will never get sql error,
  * but even if we did we don't want that to prevent them from logging out)
+ * 
+ * ---
+ * 
+ * This endpoint will return a `200` status code in all situations
  */
 export async function logout(req, res) {
 
     // If they didn't supply ANY credentials at all we don't need to run the auth check - we know they aren't logged in
-    if (!shouldAttemptAuth(req)) {
+    if (!userSuppliedAnyAuth(req)) {
         return res.status(200).json({ loggedOut: "You weren't even logged in" })
     }
 
     // With these options: authCheckHelper does not do much, besides returning existing session if it exists.
-    // Clearing cookies and responding is up to us
+    // Clearing cookies and responding is up to us.
+    // Note that sessions are always deleted on auth fail (if auth fail contained a JWT cookie with a sessionID)
     const authCheckResult = await authCheckHelper(req, res, { sendResOnFail: false, deleteCookiesOnFail: false });
 
     res.clearCookie("auth_jwt")
@@ -174,14 +216,16 @@ export async function logout(req, res) {
     // We do not need to do anything else if authCheck fails due to db error, auth rejection, session expired
     // Since we are logging out anyways. We only use authCheck to get sessionId so we can delete it manually
     if (!authCheckResult) {
-        return;
+        // If authCheck failed (i.e we got to this block) and JWT cookie existed, the referenced session is deleted automatically for safety
+        // So here we only have to respond
+        return res.status(200).json({ loggedOut: "You have successfully logged out (you had bad auth btw)" });
     }
 
-    const { sessionId } = authCheckResult;
+    const { sessionUUID } = authCheckResult;
 
     // Even if deleteSession has an SQL error we will still return 200 OK because sessions are auto deleted
     // whenever the server restarts anyways so we don't care if it failed
-    await deleteSession(sessionId)
+    deleteSessionSoon(sessionUUID)
 
     return res.status(200).json({ loggedOut: "You have successfully logged out" })
 }
@@ -189,8 +233,12 @@ export async function logout(req, res) {
 /**
  * `GET` `/auth/loggedInCheck`
  * 
+ * ---
+ * 
  * Used by the higher order component, `redirectIfLoggedIn`. Upon mounting, this component will make
  * a GET request to this endpoint. This endpoint will return an object containing ONE of the following properties:
+ * 
+ * ---
  * 
  * `notLoggedIn`: "user is not logged in"
  * 
@@ -202,15 +250,20 @@ export async function logout(req, res) {
  * something with it
  * 
  * If `loggedIn` property is returned, then the HOC will perform the redirect
+ * 
+ * ---
+ * 
+ * This endpoint always returns a `200`, although the json has 3 different informative messages
  */
 export async function loggedInCheck(req, res) {
 
     // If they didn't supply ANY credentials at all we don't need to run the auth check - we know they aren't logged in
-    if (!shouldAttemptAuth(req)) {
+    if (!userSuppliedAnyAuth(req)) {
         return res.status(200).json({ notLoggedIn: "You are not logged in" })
     }
 
     // With these options: cookies will be deleted on any form of failure (including db error) but we will deal with the response on our own
+    // Note that sessions are always deleted on auth fail (if auth fail contained a JWT cookie with a sessionID)
     const authCheckSuccess = await authCheckHelper(req, res, { sendResOnFail: false, deleteCookiesOnFail: true });
 
     if (!authCheckSuccess) {
@@ -223,12 +276,15 @@ export async function loggedInCheck(req, res) {
 // Returns { token, decoded } or false if there was an sql error
 export async function createSessionAndSetCookies(client, res) {
 
-    const session = await createSession(client)
-    if (session.sqlError) {
+    let session;
+    try {
+        session = await createSession(client);
+    }
+    catch (err) {
         return false;
     }
 
-    // Session returns [ token, decodedToken ]. We only destructure CSRF out of decodedToken here
+    // Session returns [ token, decodedToken: { sessionUUID, sessionCSRF } ]. We only destructure CSRF out of decodedToken here
     const [token, { sessionCSRF }] = session;
 
     // Session cookie lasts 3 hours while session in database lasts 15 minutes
